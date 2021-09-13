e5da4326b0032399139dd5cc34586fb81540076d
Víctor M <victormaraver@me.com>
2021-09-13 13:58:00 +0200

    d

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@
 module.exports = app => {
 
   app.use("/", require("./base.routes"))
-  app.use("/events", require("./events.routes"))
-  app.use("/news", require("./news.routes"))
-  app.use("/community", require("./community.routes"))
-  app.use("/user", require("./user.routes"))
-  app.use("/", require("./auth.routes"))
+  // app.use("/events", require("./events.routes"))
+  // app.use("/news", require("./news.routes"))
+  // app.use("/community", require("./community.routes"))
+  // app.use("/user", require("./user.routes"))
+  // app.use("/", require("./auth.routes"))
 
 }
