4a98c813fb699a369714bbbb53cb3a666bfac0db
Sara Mansori <sara.mansori.amar@gmail.com>
2021-09-13 15:24:55 +0200

    routes solved

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-const router = require("express").Router();
+
+module.exports = app => {
 
   app.use("/", require("./base.routes"))
-  // app.use("/events", require("./events.routes"))
-  // app.use("/news", require("./news.routes"))
-  // app.use("/community", require("./community.routes"))
-  // app.use("/user", require("./user.routes"))
-  // app.use("/", require("./auth.routes"))
+  //app.use("/events", require("./events.routes"))
+  //app.use("/news", require("./news.routes"))
+  //app.use("/community", require("./community.routes"))
+  //app.use("/user", require("./user.routes"))
+  //app.use("/", require("./auth.routes"))
 
-module.exports = router;
+}
